227f1075d7a2e7f1c441b3cf37e3cc005057c555
arief <arief25ramadhan@yahoo.com>
2023-07-10 20:24:33 +0700

    Initialize model class

--- model.py
+++ model.py
@@ -86,3 +86,110 @@ class MobileNetv2(nn.Module):
         outs.append(self.layer7(outs[-1]))  # 160, x / 32
         outs.append(self.layer8(outs[-1]))  # 320, x / 32
         return [outs[idx] for idx in self.return_idx]
+
+
+def make_list(x):
+    """Returns the given input as a list."""
+    if isinstance(x, list):
+        return x
+    elif isinstance(x, tuple):
+        return list(x)
+    else:
+        return [x]
+
+class CRPBlock(nn.Module):
+    """CRP definition"""
+    def __init__(self, in_planes, out_planes, n_stages, groups=False):
+        super().__init__()
+        for i in range(n_stages):
+            setattr(self, '{}_{}'.format(i + 1, 'outvar_dimred'),
+                    conv1x1(in_planes if (i == 0) else out_planes,
+                            out_planes, stride=1,
+                            bias=False, groups=in_planes if groups else 1))
+        self.stride = 1
+        self.n_stages = n_stages
+        self.maxpool = nn.MaxPool2d(kernel_size=5, stride=1, padding=2)
+
+    def forward(self, x):
+        top = x
+        for i in range(self.n_stages):
+            top = self.maxpool(top)
+            top = getattr(self, '{}_{}'.format(i + 1, 'outvar_dimred'))(top)
+            x = top + x
+        return x
+
+class MTLWRefineNet(nn.Module):
+    def __init__(self, input_sizes, num_classes, agg_size=256, n_crp=4):
+        super().__init__()
+
+        stem_convs = nn.ModuleList()
+        crp_blocks = nn.ModuleList()
+        adapt_convs = nn.ModuleList()
+        heads = nn.ModuleList()
+
+        # Reverse since we recover information from the end
+        input_sizes = list(reversed((input_sizes)))
+
+        # No reverse for collapse indices is needed
+        self.collapse_ind = [[0, 1], [2, 3], 4, 5]
+
+        groups = [False] * len(self.collapse_ind)
+        groups[-1] = True
+
+        for size in input_sizes:
+            stem_convs.append(conv1x1(size, agg_size, bias=False))
+
+        for group in groups:
+            crp_blocks.append(self._make_crp(agg_size, agg_size, n_crp, group))
+            adapt_convs.append(conv1x1(agg_size, agg_size, bias=False))
+
+        self.stem_convs = stem_convs
+        self.crp_blocks = crp_blocks
+        self.adapt_convs = adapt_convs[:-1]
+
+        num_classes = list(num_classes)
+        for n_out in num_classes:
+            heads.append(
+                nn.Sequential(
+                    conv1x1(agg_size, agg_size, groups=agg_size, bias=False),
+                    nn.ReLU6(inplace=False),
+                    conv3x3(agg_size, n_out, bias=True),
+                )
+            )
+
+        self.heads = heads
+        self.relu = nn.ReLU6(inplace=True)
+
+    def forward(self, xs):
+        xs = list(reversed(xs))
+        for idx, (conv, x) in enumerate(zip(self.stem_convs, xs)):
+            xs[idx] = conv(x)
+
+        # Collapse layers
+        c_xs = [sum([xs[idx] for idx in make_list(c_idx)]) for c_idx in self.collapse_ind ]
+
+        for idx, (crp, x) in enumerate(zip(self.crp_blocks, c_xs)):
+            if idx == 0:
+                y = self.relu(x)
+            else:
+                y = self.relu(x + y)
+            y = crp(y)
+            if idx < (len(c_xs) - 1):
+                y = self.adapt_convs[idx](y)
+                y = F.interpolate(
+                    y,
+                    size=c_xs[idx + 1].size()[2:],
+                    mode="bilinear",
+                    align_corners=True,
+                )
+
+        outs = []
+        for head in self.heads:
+            outs.append(head(y))
+        return outs
+
+    @staticmethod
+    def _make_crp(in_planes, out_planes, stages, groups):
+        # Same as previous, but showing the use of a @staticmethod
+        layers = [CRPBlock(in_planes, out_planes, stages, groups)]
+        return nn.Sequential(*layers)
