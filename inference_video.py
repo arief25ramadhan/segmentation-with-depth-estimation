d14ef16a50cfde6ec741dd5dbc7cfe767dd1b3fd
Arief Ramadhan <arief25ramadhan@yahoo.com>
2023-09-27 15:04:42 +0700

    Update inference_video.py

--- inference_video.py
+++ inference_video.py
@@ -51,3 +51,36 @@ out = cv2.VideoWriter('media/out.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 15, (w,3*
 for i in range(len(result_video)):
     out.write(result_video[i])
 out.release()
+
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture('chaplin.mp4')
+ 
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+ 
+# Read until video is completed
+while(cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
+        
+        # Display the resulting frame
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.imshow('Frame',frame)
+        
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+ 
+    # Break the loop
+    else: 
+        break
+ 
+# When everything done, release the video capture object
+cap.release()
+ 
+# Closes all the frames
+cv2.destroyAllWindows()
